a698d38725f09df106f5d1304eefbefcb9819f91
Martin Dzhonov <martin.dzhonov@gmail.com>
2016-12-02 17:57:32 +0200

    session

--- D2PickerAPI/server.js
+++ D2PickerAPI/server.js
@@ -4,14 +4,16 @@ var db = require('./libs/DBConnect.js');
 var heroes = require('./libs/heroes.json');
 var app = express();
 var Hero = require("./models/HeroModel.js")(db);
+var session = require('express-session');
 
-Hero.create({ name: 'test'});
-app.get('*', function(req, res) {
-  // return 404
-});
+var auth = function(req, res, next){ if (!req.isAuthenticated()) res.send(401); else next(); };
 
-var environment = config.env;
-var credentials = config[environment];
-app.listen(credentials.port);
+app.get('/', routes.index);
+app.get('/users', auth, user.list);
 
-console.log("Running at Port " + credentials.port);
+
+app.get('/loggedin', function(req, res) { res.send(req.isAuthenticated() ? req.user : '0'); });
+
+app.post('/login', passport.authenticate('local'), function(req, res) { res.send(req.user); });
+
+app.post('/logout', function(req, res){ req.logOut(); res.send(200); });
